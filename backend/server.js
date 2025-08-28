21f1558bcb60695dce09612ea683de5d9c7ed633
Vishal Sharma <vishal2003ggg@gmail.com>
2025-08-28 10:15:04 +0530

    connection made with mongodb

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./utils/db');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Canteen Ordering System API');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
